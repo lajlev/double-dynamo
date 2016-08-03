a9ab50c421c8a01b0d5330624657e1bbec51b702
Michael Lajlev <lajlev@gmail.com>
2016-08-03 22:27:46 +0200

    change input text to search

--- searchform.php
+++ searchform.php
@@ -1,13 +1,13 @@
-<?php $search_text = "Søg..."; ?> 
-<form method="get" id="searchform"  
-action="<?php bloginfo('home'); ?>/"> 
-<input 
-	type="text" 
-	value="<?php echo $search_text; ?>"  
-	name="s" 
-	id="s"  
-	onblur="if (this.value == '') {this.value = '<?php echo $search_text; ?>';}"  
-	onfocus="if (this.value == '<?php echo $search_text; ?>')  {this.value = '';}" 
-/> 
-<input type="hidden" id="searchsubmit" /> 
+<?php $search_text = "Søg..."; ?>
+<form method="get" id="searchform"
+action="<?php bloginfo('home'); ?>/">
+<input
+	type="search"
+	value="<?php echo $search_text; ?>"
+	name="s"
+	id="s"
+	onblur="if (this.value == '') {this.value = '<?php echo $search_text; ?>';}"
+	onfocus="if (this.value == '<?php echo $search_text; ?>')  {this.value = '';}"
+/>
+<input type="hidden" id="searchsubmit" />
 </form>
